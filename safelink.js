0f0aed9e17a886b001d52d0d7e2d05aea06c3f60
Komptik <54774733+Komptik@users.noreply.github.com>
2019-09-13 20:04:59 +0700

    Add files via upload

---
+++ safelink.js
@@ -0,0 +1,9 @@
+var x=Math.floor((Math.random()*6)+ 1);
+var xxx=null;
+if(x=="1"){xxx="beginner-guide-shopify-vs-woocommerce.html"}
+if(x=="2"){xxx="ecommerce-importance-of-seo-in-2019.html"}
+if(x=="3"){xxx="the-future-internet-multilingual-chatbot.html"}
+if(x=="4"){xxx="5-new-website-design.html"}
+if(x=="5"){xxx="3-effective-web-design-strategies.html"}
+if(x=="6"){xxx="how-website-design.html"}
+a_to_ve[i].href="https://safelink.komptik.id/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
